f9df81d919f0bd43b7836a5076d274d96adceddc
JEONG GIHOON <rlgns927@gmail.com>
2023-01-10 17:47:32 +0900

    fragmentSwipeView

---
+++ app/src/main/java/git/myapplication/fragmentswipeview/MainActivity.kt
@@ -0,0 +1,18 @@
+package git.myapplication.fragmentswipeview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val viewpagerFragmentAdapter = ViewpagerFragmentAdapter(this)
+        viewPager.adapter = viewpagerFragmentAdapter
+
+
+    }
+}
